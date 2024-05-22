8512a328e89cbf2543a0cc17eb55930f58bcce3e
Guido Gimeno <gimenoguido@gmail.com>
2024-05-22 03:46:20 -0300

    feat: ident

--- client/ipc/http.js
+++ client/ipc/http.js
@@ -1,24 +1,24 @@
 const { net, ipcMain } = require("electron")
 
 ipcMain.handle("fetch", async (_, req) => {
-    console.log("MAIN: req", req)
-    const res = await fetch(req)
-    console.log("MAIN: res", res)
-    return res
+	console.log("MAIN: req", req)
+	const res = await fetch(req)
+	console.log("MAIN: res", res)
+	return res
 })
 
 async function fetch(req) {
-    const res = await net.fetch("http://localhost:3000" + req.path, { 
-        method: req.method,
-        headers: {
-            "Content-Type": "application/json",
-            ...req.headers
-        },
-        body: JSON.stringify(req.body)
-    })
+	const res = await net.fetch("http://localhost:3000" + req.path, {
+		method: req.method,
+		headers: {
+			"Content-Type": "application/json",
+			...req.headers
+		},
+		body: JSON.stringify(req.body)
+	})
 
-    if (!res.ok) {
-    	return { status: res.status, data: null }
+	if (!res.ok) {
+		return { status: res.status, data: null }
 	}
 
 	try {
