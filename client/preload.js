a162e9ee4b435fa12437a2c6f4143a8c4f92d653
guidogimeno <gimenoguido@gmail.com>
2024-05-27 20:25:29 -0300

    linter

--- client/preload.js
+++ client/preload.js
@@ -3,6 +3,6 @@ const { contextBridge, ipcRenderer } = require("electron")
 contextBridge.exposeInMainWorld("http", {
     fetch: async (req) => {
         const res = await ipcRenderer.invoke("fetch", req)
-		return res
+        return res
     }
 })
