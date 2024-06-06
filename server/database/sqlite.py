9b14a5dde81ed04c50c33383d3a01c359da2608b
guidogimeno <gimenoguido@gmail.com>
2024-06-05 22:36:26 -0300

    upsert token

--- server/database/sqlite.py
+++ server/database/sqlite.py
@@ -5,7 +5,7 @@ from domain.session import Session
 from errors.api_exception import InternalServerError
 from errors.error_types import ErrorType
 from logger.logger import log_error
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 class MySqlite:
     def __init__(self):
@@ -41,8 +41,7 @@ class MySqlite:
             if data:
                 return Session(
                         token=data[0],
-                        expires_at=datetime.strptime(data[1], "%Y-%m-%d %H:%M:%S")
-                )
+                        expires_at=datetime.fromisoformat(data[1])                )
             else:
                 return None 
         except Exception as e:
@@ -64,13 +63,13 @@ class MySqlite:
             raise InternalServerError(ErrorType.DB_ERROR)
 
     def save_token(self, user_id, token):
+        expires_at = datetime.now(timezone.utc) + timedelta(hours=12)
+        sql = f"""INSERT INTO user_sessions (user_id, token, expires_at) VALUES (?, ?, ?)
+        ON CONFLICT (user_id) DO UPDATE SET token = excluded.token, expires_at = excluded.expires_at;"""
         try:
             with sqlite3.connect(self.database_file) as conn:
                 cursor = conn.cursor()
-                cursor.execute(
-                        "INSERT INTO user_sessions (user_id, token) VALUES (?, ?)", 
-                        (user_id, token)
-                )
+                cursor.execute(sql, (user_id, token, expires_at))
                 conn.commit()
         except Exception as e:
             log_error(f"DB: failed to save token for user {user_id}, error: {str(e)}")
