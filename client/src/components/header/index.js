f6fd67d734e2547ac97ec71905d4dbc3f4c12714
guidogimeno <gimenoguido@gmail.com>
2024-08-05 00:27:23 -0300

    feat: popup

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useRef, useState } from "react"
 import { Link } from "react-router-dom"
 import UserSvg from "../../assets/user_svg.js"
 import { GlobalContext } from "../../context/index.js"
@@ -9,14 +9,37 @@ function Header() {
     const context = useContext(GlobalContext)
     const username = context.user.name
 
+    const [open, setOpen] = useState(false)
+
+    let popupRef = useRef()
+
+    useEffect(() => {
+        let handler = function(event) {
+            if (!popupRef.current.contains(event.target)) {
+                setOpen(false)
+            }
+        }
+        document.addEventListener("mousedown", handler);
+
+        return function() {
+            document.removeEventListener("mousedown", handler)
+        }
+    }, [])
+
     return (
         <header>
             <div className="logo">
                 {username ? <Link to="/my_hips">{logo}</Link> : logo}
             </div>
-            <div className="user">
-                <UserSvg />
+            <div className="user" >
+                <UserSvg onClick={() => setOpen(prev => !prev)} />
                 <p>{username}</p>
+                <div className={`user-popup ${open ? "active" : "inactive"}`} popupRef={popupRef}>
+                    <p>{username}</p>
+                    <button onClick={() => console.log("profile")}>My Profile</button>
+                    <button onClick={() => console.log("edit")}>Edit Profile</button>
+                    <button onClick={() => console.log("logout")}>Logout</button>
+                </div>
             </div>
         </header>
     )
