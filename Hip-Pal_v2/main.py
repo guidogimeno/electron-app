3fd9f837385af94fb1467588645b4f5809bd7db8
Guido Gimeno <36056989+guidogimeno@users.noreply.github.com>
2024-09-03 20:31:54 -0300

    Revert "rebase > merge (#14)"
    This reverts commit 3108b42c7116dc23944f2c5416dbf507feb1efd0.

--- Hip-Pal_v2/main.py
+++ Hip-Pal_v2/main.py
@@ -4,74 +4,63 @@ from Excepciones.Excepciones import ErrorDetectandoAngulos, ErrorIntermedialNotF
 import SectorAcetabular.sectorAcetabular as sectorAcetabular
 import SectorAcetabular.Utils.detectar as detectar
 import json
-from TCtoNiigz import dcm_to_nii
-from Inferencia import segmentar
 
 
 def main():
-    print("GUIDO DEBUG: empieza")
-    dcm_to_nii()
-    print("GUIDO DEBUG: sali de aca")
+    try:
+        ruta_json_resultados = r'C:\Users\Usuario\anaconda3\envs\monailabel-env\pruebas\angulos.json'
 
-    # predecimos
-    print("GUIDO DEBUG: empieza la segmentacion")
-    segmentar()
-    print("GUIDO DEBUG: segmentacion finalizada")
 
-    # try:
-    #     ruta_json_resultados = r'C:\Users\Usuario\anaconda3\envs\monailabel-env\pruebas\angulos.json'
-    #
-    #
-    #     #Deberia ir aca la Prediccion------------------------------------------------------------------------
-    #     # Cargar la tomografía y la máscara
-    #     tomografia_original_path = os.path.join('Tomografia', 'Original', 'CT_9.nii.gz')
-    #     tomografia_original = nib.load(tomografia_original_path).get_fdata()
-    #
-    #     # Cargar prediccion----------------------------------------------------------------------------------
-    #     tomografia_segmentada_path = os.path.join('Tomografia', 'Prediccion', 'CT_9_seg.nii.gz')
-    #     tomografia_segmentada = nib.load(tomografia_segmentada_path).get_fdata()
-    #
-    #
-    #     #Verificacion de etiquetas--------------------------------------------------------
-    #     # Verifica si la máscara tiene múltiples etiquetas
-    #     if tomografia_segmentada.ndim != 4 and tomografia_segmentada.shape[3] != 8:
-    #         raise ErrorCantidadEtiquetas(
-    #         "La máscara no tiene el formato esperado de 8 etiquetas.",
-    #         detalles=f"Dimensiones: {tomografia_segmentada.ndim}, Shape: {tomografia_segmentada.shape}"
-    #     )
-    #
-    #
-    #     #Detecto cabeza femur en el axial (Proximal, Intermedial y Ecuatorial)-------------
-    #     cabezas_femur_axiales = sectorAcetabular.detectar(tomografia_segmentada)
-    #
-    #     #Detecta angulos sector Acetabular-------------------------------------------------
-    #     angulosSectorAcetabular = detectar.detectar(cabezas_femur_axiales,tomografia_original,tomografia_segmentada)
-    #
-    #     # Guardo el JSON en el archivo------------------------------------------------------
-    #     with open(ruta_json_resultados, 'w') as archivo:
-    #         json.dump(angulosSectorAcetabular, archivo, indent=4)  # indent=4 para un formato legible
-    #
-    #     print("Termino: 200")
-    #     return 200
-    # except ErrorCantidadEtiquetas as e:
-    #     print(f"Error: {e}")
-    #     print(f"Detalles adicionales: {e.detalles}")
-    #     print("Termino: 404")
-    #     return 404
-    # except ErrorProximalEcuatorialNotFound as e:
-    #     print(f"Error: {e}")
-    #     print(f"Detalles adicionales: {e.detalles}")
-    #     print("Termino: 500")
-    #     return 500
-    # except ErrorIntermedialNotFound as e:
-    #     print(f"Error: {e}")
-    #     print(f"Detalles adicionales: {e.detalles}")
-    #     print("Termino: 500")
-    #     return 500
-    # except ErrorDetectandoAngulos as e:
-    #     print(f"Error: {e}")
-    #     print("Termino: 500")
-    #     return 500
+        #Deberia ir aca la Prediccion------------------------------------------------------------------------
+        # Cargar la tomografía y la máscara
+        tomografia_original_path = os.path.join('Tomografia', 'Original', 'CT_9.nii.gz')
+        tomografia_original = nib.load(tomografia_original_path).get_fdata()
+
+        # Cargar prediccion----------------------------------------------------------------------------------
+        tomografia_segmentada_path = os.path.join('Tomografia', 'Prediccion', 'CT_9_seg.nii.gz')
+        tomografia_segmentada = nib.load(tomografia_segmentada_path).get_fdata()
+
+
+        #Verificacion de etiquetas--------------------------------------------------------
+        # Verifica si la máscara tiene múltiples etiquetas
+        if tomografia_segmentada.ndim != 4 and tomografia_segmentada.shape[3] != 8:
+            raise ErrorCantidadEtiquetas(
+            "La máscara no tiene el formato esperado de 8 etiquetas.",
+            detalles=f"Dimensiones: {tomografia_segmentada.ndim}, Shape: {tomografia_segmentada.shape}"
+        )
+
+
+        #Detecto cabeza femur en el axial (Proximal, Intermedial y Ecuatorial)-------------
+        cabezas_femur_axiales = sectorAcetabular.detectar(tomografia_segmentada)
+
+        #Detecta angulos sector Acetabular-------------------------------------------------
+        angulosSectorAcetabular = detectar.detectar(cabezas_femur_axiales,tomografia_original,tomografia_segmentada)
+
+        # Guardo el JSON en el archivo------------------------------------------------------
+        with open(ruta_json_resultados, 'w') as archivo:
+            json.dump(angulosSectorAcetabular, archivo, indent=4)  # indent=4 para un formato legible
+
+        print("Termino: 200")
+        return 200
+    except ErrorCantidadEtiquetas as e:
+        print(f"Error: {e}")
+        print(f"Detalles adicionales: {e.detalles}")
+        print("Termino: 404")
+        return 404
+    except ErrorProximalEcuatorialNotFound as e:
+        print(f"Error: {e}")
+        print(f"Detalles adicionales: {e.detalles}")
+        print("Termino: 500")
+        return 500
+    except ErrorIntermedialNotFound as e:
+        print(f"Error: {e}")
+        print(f"Detalles adicionales: {e.detalles}")
+        print("Termino: 500")
+        return 500
+    except ErrorDetectandoAngulos as e:
+        print(f"Error: {e}")
+        print("Termino: 500")
+        return 500
 
 
 
---
+++ scripts/findAndTransform/TCtoNiigz.py
@@ -0,0 +1,107 @@
+import os
+import pydicom
+from pydicom.multival import MultiValue
+import argparse
+import subprocess
+
+# Devuelve el top x de carpetas con más archivos
+def top_folders_by_file_count(root_folder, amount):
+    folder_file_count = {}
+    
+    with os.scandir(root_folder) as it:
+        for entry in it:
+            if entry.is_dir():
+                folder_file_count[entry.name] = len(os.listdir(entry.path))
+    
+    # Ordena las carpetas por la cantidad de archivos de mayor a menor y selecciona las top 'amount'
+    top_folders = sorted(folder_file_count, key=folder_file_count.get, reverse=True)[:amount]
+    
+    return top_folders
+
+# Obtiene los valores Series Description y Window Width de la metadata
+def get_metadata(folder_path):
+    try:
+        first_file_path = next(os.scandir(folder_path)).path
+        # Leer metadata del primer archivo
+        metadata = pydicom.dcmread(first_file_path)
+        # Acceder a los atributos específicos
+        series_description = metadata.get('SeriesDescription', "No disponible")
+        window_width = metadata.get('WindowWidth', "No disponible")
+    except Exception:
+        return "No disponible", "No disponible"
+    
+    return series_description, window_width
+
+# Determina si la serie contiene la palabra "bone" o "hueso"
+def is_contain_bone(series_description):
+    return "bone" in series_description.lower() or "hueso" in series_description.lower()
+
+# Evalúa si window_width es >= 1500
+def is_between_values(window_width):
+    if isinstance(window_width, MultiValue):
+        return any(width >= 1500 for width in window_width)
+    return isinstance(window_width, (int, float)) and window_width >= 1500
+
+# Devuelve el path de la carpeta a utilizar en las mediciones
+def get_folder_TC(root_folder):
+    top_folders = top_folders_by_file_count(root_folder, 3)
+
+    for folder_name in top_folders:
+        folder_path = os.path.join(root_folder, folder_name)
+        series_description, window_width = get_metadata(folder_path)
+
+        if is_contain_bone(series_description) or is_between_values(window_width):
+            return folder_path
+    return None
+
+# Valida si un archivo pertenece a una tomografía
+# El valor 1.2.840.10008.1.3.10 solo esta presente en un DICOMDIR, se valida que no tenga ese valor
+def is_ct_dicom_file(file_path):
+    try:
+        dcm = pydicom.dcmread(file_path, stop_before_pixels=True)
+        return dcm.file_meta.MediaStorageSOPClassUID != '1.2.840.10008.1.3.10'
+    except Exception:
+        return False
+
+# Recorre los directorios y subdirectorios, y devuelve el path de la carpeta a utilizar en las mediciones
+def get_final_path_CT(directorio):
+    for root, _, files in os.walk(directorio):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if is_ct_dicom_file(file_path):
+                return get_folder_TC(os.path.dirname(root))
+    return "La carpeta seleccionada no cumple con el formato de una tomografía"
+
+# Transforma un .dcm a un .nii.gz
+def convert_dcm_to_nii(dicom_folder, output_folder):
+    # Verifica que el directorio de salida exista
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Comando para convertir de DICOM a NIfTI
+    command = f'dcm2niix -z y -o {output_folder} {dicom_folder}'
+    
+    # Ejecutar el comando
+    subprocess.run(command, shell=True, check=True)
+
+    # -z y: compress the output NIfTI file into .nii.gz
+    # -o: specify the output directory
+
+#--------------------------------------------------------------------------------------------------
+
+# Función principal
+def main():
+    parser = argparse.ArgumentParser(description="Script para encontrar el directorio de tomografía a partir de una ruta y luego convertirlo a .nii.gz")
+    parser.add_argument("ruta", help="Ruta del directorio raíz para iniciar la búsqueda.")
+    parser.add_argument("carpeta_de_salida", help="Ruta a la carpeta donde quedara el .nii.gz")
+    args = parser.parse_args()
+
+    selectedPath = get_final_path_CT(args.ruta)
+    print(selectedPath)
+
+    dicom_folder = selectedPath
+    output_folder = args.carpeta_de_salida
+    convert_dcm_to_nii(dicom_folder, output_folder)
+
+if __name__ == "__main__":
+    main()
