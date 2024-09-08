8d4af2e901c80bd2615210229048c86afc5ba8e5
guidogimeno <gimenoguido@gmail.com>
2024-09-08 20:12:14 -0300

    cambios webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,41 +5,42 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default {
-  mode: "development",
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-  target: "electron-renderer",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [[
-              "@babel/preset-env", {
-                targets: {
-                  esmodules: true
+    mode: "development",
+    entry: "./src/index.js",
+    devtool: "inline-source-map",
+    target: "electron-renderer",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [[
+                            "@babel/preset-env", {
+                                targets: {
+                                    esmodules: true
+                                }
+                            }],
+                            "@babel/preset-react"]
+                    }
                 }
-              }],
-              "@babel/preset-react"]
-          }
-        }
-      },
-      {
-        test: [/\.css$/i],
-        use: [
-          "css-loader",
-        ],
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js"],
-  },
-  output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "build", "js"),
-  },
+            },
+            {
+                test: [/\.css$/i],
+                use: [
+                    "style-loader",
+                    "css-loader",
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js"],
+    },
+    output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, "build", "js"),
+    },
 }
