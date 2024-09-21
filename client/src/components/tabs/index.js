83dd3c4cc7c8fd50420ef9bbc4cc985cf4d63847
Guido Gimeno <36056989+guidogimeno@users.noreply.github.com>
2024-09-21 15:28:48 -0300

    Feature/cambios2 (#25)
    * se ve un poco mejor
    
    * cambios
    
    * cambios
    
    * cambios

---
+++ client/src/components/tabs/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+function Tabs({ children }) {
+    const [selectedTab, setSelectedTab] = useState(0);
+
+    function handleTabClick(index) {
+        setSelectedTab(index);
+    };
+
+    return (
+        <div className="tabs">
+            <TabList tabs={children} selectedTab={selectedTab} onTabClick={handleTabClick} />
+            <TabContent tabs={children} selectedTab={selectedTab} />
+        </div>
+    );
+}
+
+function TabList({ tabs, selectedTab, onTabClick }) {
+    return (
+        <ul className="tab-list">
+            {tabs.map((tab, index) => (
+                <TabTrigger key={index} tab={tab} selected={selectedTab === index} onClick={() => onTabClick(index)} />
+            ))}
+        </ul>
+    );
+}
+
+
+function TabTrigger({ tab, selected, onClick }) {
+    return (
+        <li className={`tab-trigger ${selected ? 'active' : ''}`} onClick={onClick}>
+            {tab.props.label}
+        </li>
+    );
+}
+
+
+function TabContent({ tabs, selectedTab }) {
+    console.log(tabs)
+    return (
+        <div className="tab-content">
+            {tabs.map((tab, index) => (
+                <div key={index} className={`tab-pane ${selectedTab === index ? 'active' : ''}`}>
+                    {tab.props.children}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+function Tab({ children, label }) {
+    return {
+        label,
+        content: children,
+    };
+}
+
+export default Tab;
+
+export {
+    Tabs,
+    Tab,
+    TabList,
+    TabTrigger,
+    TabContent
+}
