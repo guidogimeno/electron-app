d4ca9f2154f688024dc23150e567241978fa1ab4
guidogimeno <gimenoguido@gmail.com>
2024-09-29 14:13:58 -0300

    cambios

--- client/ipc/bin.js
+++ client/ipc/bin.js
@@ -4,23 +4,26 @@ import { spawn } from "child_process"
 const userDataPath = app.getPath("userData")
 
 ipcMain.handle("execute", async (_, filePath) => {
-    console.log(`about to execute ${filePath}`)
+    console.log(`FILEPATH ${filePath}`)
+    console.log(`USERDATAPATH ${userDataPath}`)
     return new Promise(function(resolve, reject) {
         const binary = spawn(`${userDataPath}/bin/main`, [`${filePath}`, `${userDataPath}`]);
 
-        binary.stdout.on("data", (result) => {
-            const id = String(result).split("hippal_stdout:$")[1]
+        binary.stdout.on("data", (data) => {
+            console.log(`LOG INFO - stdout: ${data}`)
+            const id = String(data).split("hippal_stdout:$")[1]
             if (id) {
                 console.log(`Id generado: ${id}`)
                 resolve(id)
             }
         })
 
-        binary.on("close", (result) => {
-            console.log(`Cerrando ejecutable: ${result}`)
+        binary.on("close", (data) => {
+            console.log(`LOG INFO - close: ${data}`)
         })
 
         binary.stderr.on("data", (data) => {
+            console.log(`LOG INFO - stderr: ${data}`)
             const errorMessage = String(data).split("hippal_stderr:$")[1]
             if (errorMessage) {
                 reject(new Error(`process exited with code ${errorMessage}`))
