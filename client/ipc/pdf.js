2fdddec8c2f70591807904c50e9e4f92b8886dca
Guido Gimeno <36056989+guidogimeno@users.noreply.github.com>
2024-10-07 20:13:45 -0300

    Feature/pdf (#48)
    * pruebas
    
    * crear pdf funca
    
    ---------
    
    Co-authored-by: ggimeno_meli <guido.gimeno@mercadolibre.com>

--- client/ipc/pdf.js
+++ client/ipc/pdf.js
@@ -1,11 +1,25 @@
 import { ipcMain } from "electron"
 import puppeteer from "puppeteer"
+import os from "os"
+import path from "path"
+
+const downloadsPath = path.join(os.homedir(), "Downloads")
 
 ipcMain.handle("generate-pdf", async (_, content) => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    const page = await browser.newPage();
-    await page.setContent(content);
-    await page.pdf({ path: 'example.pdf', format: 'A4' });
-    await browser.close();
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setContent(content, { waitUntil: "domcontentloaded" })
+    await page.pdf({
+        format: "A4",
+        path: `${downloadsPath}/hippal_report-${Date.now().toString()}.pdf`,
+        printBackground: true,
+        margin: {
+            top: "20px",
+            bottom: "20px",
+            left: "20px",
+            right: "20px"
+        }
+    })
+    await browser.close()
 })
 
