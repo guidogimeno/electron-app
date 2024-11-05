c195f2cb96749694aeae7543bb0a8b5b4ba30a8b
ggimeno_meli <guido.gimeno@mercadolibre.com>
2024-11-05 20:38:47 -0300

    mi path

--- client/ipc/bin.js
+++ client/ipc/bin.js
@@ -7,7 +7,8 @@ ipcMain.handle("execute", async (_, filePath) => {
     console.log(`FILEPATH ${filePath}`)
     console.log(`USERDATAPATH ${userDataPath}`)
     return new Promise(function(resolve, reject) {
-        const binary = spawn("python", ["D:\\Usuarios\\Usuario\\Desktop\\electron-app\\Hip-Pal_v2\\main.py", `${filePath}`, `${userDataPath}`]);
+        const binary = spawn("python3", ["/Users/ggimeno/Documents/personal/electron-app/Hip-Pal_v2/main.py", `${filePath}`, `${userDataPath}`]);
+
 
         binary.stdout.on("data", (data) => {
             console.log(`LOG INFO - stdout: ${data}`)
